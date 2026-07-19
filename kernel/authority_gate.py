b9a1ba1a46f249519ed4e85347b777e56029f574
pere000 <85168793+pere000@users.noreply.github.com>
2026-07-19 05:29:13 +0100

    Add initial kernel skeleton

---
+++ kernel/authority_gate.py
@@ -0,0 +1,10 @@
+"""AuthorityGate: determines the governing authority for a classified query.
+
+Implementation pending.
+"""
+
+class AuthorityGate:
+    """Routes classified queries to their governing authority."""
+
+    def resolve(self, classification):
+        raise NotImplementedError
