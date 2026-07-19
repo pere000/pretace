1eeb9a861a6798645f48a0f1243d7fb52904041a
pere000 <85168793+pere000@users.noreply.github.com>
2026-07-19 05:29:22 +0100

    Add initial kernel skeleton

---
+++ kernel/repository_manager.py
@@ -0,0 +1,8 @@
+"""RepositoryManager: retrieves authoritative resources.
+
+Implementation pending.
+"""
+
+class RepositoryManager:
+    def fetch(self, authority_result):
+        raise NotImplementedError
